b33ab794db91a8552213d1375fde173ccce48b21
Pawel Ratajczak <pawel@lampdevelopment.co.uk>
2014-09-16 17:40:24 +0100

    Attach autosubmit js code to exposed views fileter form

---
+++ modules/features/isb_standard/js/views_exposed_form.js
@@ -0,0 +1,16 @@
+(function($) {
+
+    $submit = function() {
+        $form = $(this).parents("form").submit();
+    }
+
+    Drupal.behaviors.searchSortOrder = {
+        attach: function (context, settings) {
+            $("#edit-sort-by").change($submit);
+            $("#edit-sort-order").change($submit);
+            //code ends
+        }
+    };
+})(jQuery);
+
+
